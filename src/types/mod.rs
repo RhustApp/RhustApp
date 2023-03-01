864621bedd830c7a45261df821ac4341cd877859
Akshett Rai Jindal <akshett.jindal@research.iiit.ac.in>
2023-03-02 00:53:42 +0530

    [Add] presence types

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -10,5 +10,8 @@ pub use jid::*;
 mod message;
 pub use message::*;
 
+mod presence;
+pub use presence::*;
+
 mod user;
 pub use user::*;
---
+++ src/types/presence.rs
@@ -0,0 +1,57 @@
+use std::str::FromStr;
+
+use crate::RhustAppError;
+
+pub enum Presence {
+    Available,
+    Unavailable,
+    Value(String),
+}
+
+impl FromStr for Presence {
+    type Err = RhustAppError;
+
+    fn from_str(input: &str) -> Result<Self, RhustAppError> {
+        match input {
+            "available" => Ok(Self::Available),
+            "unavailable" => Ok(Self::Unavailable),
+            _ => Ok(Self::Value(input.to_string())),
+        }
+    }
+}
+
+pub enum ChatPresence {
+    Composing,
+    Paused,
+    Value(String),
+}
+
+impl FromStr for ChatPresence {
+    type Err = RhustAppError;
+
+    fn from_str(input: &str) -> Result<Self, RhustAppError> {
+        match input {
+            "composing" => Ok(Self::Composing),
+            "paused" => Ok(Self::Paused),
+            _ => Ok(Self::Value(input.to_string())),
+        }
+    }
+}
+
+pub enum ChatPresenceMedia {
+    Text,
+    Audio,
+    Value(String),
+}
+
+impl FromStr for ChatPresenceMedia {
+    type Err = RhustAppError;
+
+    fn from_str(input: &str) -> Result<Self, RhustAppError> {
+        match input {
+            "" => Ok(Self::Text),
+            "audio" => Ok(Self::Audio),
+            _ => Ok(Self::Value(input.to_string())),
+        }
+    }
+}
